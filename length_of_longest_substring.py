a7d30c565e548e569988368f6507fb8d06e0841c
Lina-Liuna <liuna.lina@gmail.com>
2018-11-03 20:29:36 -0700

    why do we need start <= usedChar[s[i]] # This is because once we need update start that means we want to make sure that there is no repeated char # between the s[start] and the s[i], # So if we try to judge if adding a new coming char s[i] will incur the change of start, # We should judge that if s[i] is already existed in the usedChar # and that s[i] exisited after s[start].

--- length_of_longest_substring.py
+++ length_of_longest_substring.py
@@ -7,17 +7,27 @@ class Solution:
         usedChar = {}
 
         for i in range(len(s)):
-            print(i)
+
+
             if s[i] in usedChar and start <= usedChar[s[i]]:
+                print("s[" + str(i) + "] = " + s[i] + ", start = " + str(start))
                 start = usedChar[s[i]] + 1
             else:
                 maxLength = max(maxLength, i - start + 1)
 
             usedChar[s[i]] = i
+            print("usedChar[" + s[i] + "] = " + str(usedChar[s[i]]))
 
         return maxLength
 
 
-myStr = "pwwkew"
+myStr = "tmmzuxta"
 len = Solution.lengthOfLongestSubstring(Solution, myStr)
 print(len)
+
+# why do we need start <= usedChar[s[i]]
+# This is because once we need update start that means we want to make sure that there is no repeated char
+# between the s[start] and the s[i],
+# So if we try to judge if adding a new coming char s[i] will incur the change of start,
+# We should judge that if s[i] is already existed in the usedChar
+# and that s[i] exisited after s[start].
