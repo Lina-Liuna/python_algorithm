e53f463536c2ba400ebc9c6b919bb4134a628922
Lina-Liuna <liuna.lina@gmail.com>
2018-11-08 11:14:49 -0800

    #Given a strings, find the longest palindromic substring ins. #You may assume that the maximum length ofs is 1000 #Example 1: #Input: " babad "Output: "bab, "aba" is also the valid answer

---
+++ longest_panlindromic_substring.py
@@ -0,0 +1,4 @@
+#Given a strings, find the longest palindromic substring ins.
+#You may assume that the maximum length ofs is 1000
+#Example 1:
+#Input: " babad "Output: "bab, "aba" is also the valid answer
